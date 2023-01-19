6a9194779698e866f2130667bf17c1f6a2e7492d
zaoivan <zaoivan@gmail.com>
2023-01-19 14:09:04 +0600

    fix: src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    for (let i = 0; i < n.toString().length; i++) {
+        if (n.toString()[i] === '-') continue;
+        result = `${n.toString()[i]}${result}`;
+    }
+    return result;
 }
